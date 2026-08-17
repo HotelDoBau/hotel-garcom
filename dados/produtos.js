13593f209843a2e9e2398930088f86d649a3d5d8
HotelDoBau <reservashotelrecepcao@gmail.com>
2026-08-17 09:41:38 -0300

    Update produtos.js

--- dados/produtos.js
+++ dados/produtos.js
@@ -113,186 +113,443 @@ const produtos = [
         categoria: "Cafeteria",
         preco: 12.00
     },
-// 🥐 SALGADOS
-
-{
-    id: 19,
-    nome: "Salgado assado tortinha de costela",
-    categoria: "Salgados",
-    preco: 7.50
-},
-
-{
-    id: 20,
-    nome: "Salgado assado esfiha de carne",
-    categoria: "Salgados",
-    preco: 7.50
-},
-
-{
-    id: 21,
-    nome: "Salgado assado esfiha de frango",
-    categoria: "Salgados",
-    preco: 7.50
-},
-
-{
-    id: 22,
-    nome: "Salgado assado ghirella de pizza",
-    categoria: "Salgados",
-    preco: 7.50
-},
-
-{
-    id: 23,
-    nome: "Salgado assado presunto e queijo",
-    categoria: "Salgados",
-    preco: 7.50
-},
-
-{
-    id: 24,
-    nome: "Salgado assado X-Burguer com bacon",
-    categoria: "Salgados",
-    preco: 10.00
-},
-
-{
-    id: 25,
-    nome: "Croissant pizza",
-    categoria: "Salgados",
-    preco: 7.50
-},
-
-{
-    id: 26,
-    nome: "Croissant frango com requeijão",
-    categoria: "Salgados",
-    preco: 7.50
-},
-
-{
-    id: 27,
-    nome: "Croissant de chocolate",
-    categoria: "Salgados",
-    preco: 7.50
-},
-
-{
-    id: 28,
-    nome: "Croissant de doce de leite",
-    categoria: "Salgados",
-    preco: 7.50
-},
 
 
-// 🥪 LANCHES
+    // 🥐 SALGADOS
 
-{
-    id: 29,
-    nome: "Lanche de carne louca",
-    categoria: "Lanches",
-    preco: 23.00
-},
+    {
+        id: 19,
+        nome: "Salgado assado tortinha de costela",
+        categoria: "Salgados",
+        preco: 7.50
+    },
 
-{
-    id: 30,
-    nome: "Lanche de ragu",
-    categoria: "Lanches",
-    preco: 25.00
-},
+    {
+        id: 20,
+        nome: "Salgado assado esfiha de carne",
+        categoria: "Salgados",
+        preco: 7.50
+    },
 
-{
-    id: 31,
-    nome: "Lanche de frango empanado",
-    categoria: "Lanches",
-    preco: 22.00
-},
+    {
+        id: 21,
+        nome: "Salgado assado esfiha de frango",
+        categoria: "Salgados",
+        preco: 7.50
+    },
 
-{
-    id: 32,
-    nome: "Lanche natural",
-    categoria: "Lanches",
-    preco: 14.00
-},
+    {
+        id: 22,
+        nome: "Salgado assado ghirella de pizza",
+        categoria: "Salgados",
+        preco: 7.50
+    },
 
-{
-    id: 33,
-    nome: "Misto quente",
-    categoria: "Lanches",
-    preco: 15.00
-},
+    {
+        id: 23,
+        nome: "Salgado assado presunto e queijo",
+        categoria: "Salgados",
+        preco: 7.50
+    },
+
+    {
+        id: 24,
+        nome: "Salgado assado X-Burguer com bacon",
+        categoria: "Salgados",
+        preco: 10.00
+    },
+
+    {
+        id: 25,
+        nome: "Croissant pizza",
+        categoria: "Salgados",
+        preco: 7.50
+    },
+
+    {
+        id: 26,
+        nome: "Croissant frango com requeijão",
+        categoria: "Salgados",
+        preco: 7.50
+    },
+
+    {
+        id: 27,
+        nome: "Croissant de chocolate",
+        categoria: "Salgados",
+        preco: 7.50
+    },
+
+    {
+        id: 28,
+        nome: "Croissant de doce de leite",
+        categoria: "Salgados",
+        preco: 7.50
+    },
 
 
-// 🍽️ PRATOS FEITOS
+    // 🥪 LANCHES
 
-{
-    id: 34,
-    nome: "Nhoque com molho vermelho",
-    categoria: "Pratos feitos",
-    preco: 19.90
-},
+    {
+        id: 29,
+        nome: "Lanche de carne louca",
+        categoria: "Lanches",
+        preco: 23.00
+    },
 
-{
-    id: 35,
-    nome: "Nhoque com molho bechamel",
-    categoria: "Pratos feitos",
-    preco: 19.90
-},
+    {
+        id: 30,
+        nome: "Lanche de ragu",
+        categoria: "Lanches",
+        preco: 25.00
+    },
 
-{
-    id: 36,
-    nome: "Salada Grande",
-    categoria: "Pratos feitos",
-    preco: 10.00
-},
+    {
+        id: 31,
+        nome: "Lanche de frango empanado",
+        categoria: "Lanches",
+        preco: 22.00
+    },
 
-{
-    id: 37,
-    nome: "Arroz biro-biro",
-    categoria: "Pratos feitos",
-    preco: 21.90
-},
+    {
+        id: 32,
+        nome: "Lanche natural",
+        categoria: "Lanches",
+        preco: 14.00
+    },
 
-{
-    id: 38,
-    nome: "Arroz carreteiro light",
-    categoria: "Pratos feitos",
-    preco: 29.90
-},
+    {
+        id: 33,
+        nome: "Misto quente",
+        categoria: "Lanches",
+        preco: 15.00
+    },
 
-{
-    id: 39,
-    nome: "Estrogonofe de frango",
-    categoria: "Pratos feitos",
-    preco: 27.90
-},
 
-{
-    id: 40,
-    nome: "Estrogonofe de carne",
-    categoria: "Pratos feitos",
-    preco: 29.90
-},
+    // 🍽️ PRATOS FEITOS
 
-{
-    id: 41,
-    nome: "Arroz com feijão e frango empanado",
-    categoria: "Pratos feitos",
-    preco: 21.90
-},
+    {
+        id: 34,
+        nome: "Nhoque com molho vermelho",
+        categoria: "Pratos feitos",
+        preco: 19.90
+    },
 
-{
-    id: 42,
-    nome: "Arroz com feijão e ragú ou lagarto",
-    categoria: "Pratos feitos",
-    preco: 22.90
-},
+    {
+        id: 35,
+        nome: "Nhoque com molho bechamel",
+        categoria: "Pratos feitos",
+        preco: 19.90
+    },
 
-{
-    id: 43,
-    nome: "Arroz com feijão e ovo",
-    categoria: "Pratos feitos",
-    preco: 19.90
-},
-    ];
+    {
+        id: 36,
+        nome: "Salada Grande",
+        categoria: "Pratos feitos",
+        preco: 10.00
+    },
+
+    {
+        id: 37,
+        nome: "Arroz biro-biro",
+        categoria: "Pratos feitos",
+        preco: 21.90
+    },
+
+    {
+        id: 38,
+        nome: "Arroz carreteiro light",
+        categoria: "Pratos feitos",
+        preco: 29.90
+    },
+
+    {
+        id: 39,
+        nome: "Estrogonofe de frango",
+        categoria: "Pratos feitos",
+        preco: 27.90
+    },
+
+    {
+        id: 40,
+        nome: "Estrogonofe de carne",
+        categoria: "Pratos feitos",
+        preco: 29.90
+    },
+
+    {
+        id: 41,
+        nome: "Arroz com feijão e frango empanado",
+        categoria: "Pratos feitos",
+        preco: 21.90
+    },
+
+    {
+        id: 42,
+        nome: "Arroz com feijão e ragú ou lagarto",
+        categoria: "Pratos feitos",
+        preco: 22.90
+    },
+
+    {
+        id: 43,
+        nome: "Arroz com feijão e ovo",
+        categoria: "Pratos feitos",
+        preco: 19.90
+    },
+
+
+    // 🥤 BEBIDAS
+
+    {
+        id: 44,
+        nome: "Água Minaura sem gás",
+        categoria: "Bebidas",
+        preco: 3.00,
+        imagem: "img/agua-minaura-sem-gas.webp"
+    },
+
+    {
+        id: 45,
+        nome: "Água Minaura com gás",
+        categoria: "Bebidas",
+        preco: 4.50,
+        imagem: "img/agua-minaura-com-gas.webp"
+    },
+
+    {
+        id: 46,
+        nome: "Água Crystal sem gás",
+        categoria: "Bebidas",
+        preco: 4.00,
+        imagem: "img/agua-crystal-sem-gas.webp"
+    },
+
+    {
+        id: 47,
+        nome: "Água Minalice sem gás",
+        categoria: "Bebidas",
+        preco: 3.00,
+        imagem: "img/agua-minalice-sem-gas.webp"
+    },
+
+    {
+        id: 48,
+        nome: "Água Minalice com gás",
+        categoria: "Bebidas",
+        preco: 4.50,
+        imagem: "img/agua-minalice-com-gas.webp"
+    },
+
+    {
+        id: 49,
+        nome: "Suco Nativo Frutas Cítricas",
+        categoria: "Bebidas",
+        preco: 4.00,
+        imagem: "img/suco-nativo-frutas-citricas.webp"
+    },
+
+    {
+        id: 50,
+        nome: "Suco Nativo Uva",
+        categoria: "Bebidas",
+        preco: 4.00,
+        imagem: "img/suco-nativo-uva.webp"
+    },
+
+    {
+        id: 51,
+        nome: "Suco Nativo Laranja com Acerola",
+        categoria: "Bebidas",
+        preco: 4.00,
+        imagem: "img/suco-nativo-laranja-acerola.webp"
+    },
+
+    {
+        id: 52,
+        nome: "Guaraná Antarctica lata 350ml",
+        categoria: "Bebidas",
+        preco: 7.00,
+        imagem: "img/guarana-antartica-lata.webp"
+    },
+
+    {
+        id: 53,
+        nome: "Guaraná Antarctica Zero lata 350ml",
+        categoria: "Bebidas",
+        preco: 7.00,
+        imagem: "img/guarana-antartica-zero-lata.webp"
+    },
+
+    {
+        id: 54,
+        nome: "Guaraná Antarctica caçulinha 220ml",
+        categoria: "Bebidas",
+        preco: 5.00,
+        imagem: "img/guarana-antartica-caculinha.webp"
+    },
+
+    {
+        id: 55,
+        nome: "Guaraná Antarctica Zero caçulinha 220ml",
+        categoria: "Bebidas",
+        preco: 5.00,
+        imagem: "img/guarana-antartica-zero-caculinha.webp"
+    },
+
+    {
+        id: 56,
+        nome: "Coca-Cola lata 350ml",
+        categoria: "Bebidas",
+        preco: 7.00,
+        imagem: "img/coca-cola-lata.webp"
+    },
+
+    {
+        id: 57,
+        nome: "Coca-Cola Zero lata 350ml",
+        categoria: "Bebidas",
+        preco: 7.00,
+        imagem: "img/coca-cola-zero-lata.webp"
+    },
+
+    {
+        id: 58,
+        nome: "Coca-Cola caçulinha 220ml",
+        categoria: "Bebidas",
+        preco: 5.00,
+        imagem: "img/coca-cola-caculinha.webp"
+    },
+
+    {
+        id: 59,
+        nome: "Coca-Cola Zero caçulinha 220ml",
+        categoria: "Bebidas",
+        preco: 5.00,
+        imagem: "img/coca-cola-zero-caculinha.webp"
+    },
+
+    {
+        id: 60,
+        nome: "Energético Monster Mango Loco 473ml",
+        categoria: "Bebidas",
+        preco: 13.00,
+        imagem: "img/monster-mango-loco.webp"
+    },
+
+    {
+        id: 61,
+        nome: "Energético Monster Tradicional 473ml",
+        categoria: "Bebidas",
+        preco: 13.00,
+        imagem: "img/monster-tradicional.webp"
+    },
+
+    {
+        id: 62,
+        nome: "Fanta Laranja lata 350ml",
+        categoria: "Bebidas",
+        preco: 7.00,
+        imagem: "img/fanta-laranja.webp"
+    },
+
+    {
+        id: 63,
+        nome: "Fanta Uva lata 350ml",
+        categoria: "Bebidas",
+        preco: 7.00,
+        imagem: "img/fanta-uva.webp"
+    },
+
+    {
+        id: 64,
+        nome: "Schweppes Citrus lata 350ml",
+        categoria: "Bebidas",
+        preco: 7.00,
+        imagem: "img/schweppes-citrus.webp"
+    },
+
+    {
+        id: 65,
+        nome: "Schweppes Tônica lata 350ml",
+        categoria: "Bebidas",
+        preco: 7.00,
+        imagem: "img/schweppes-tonica.webp"
+    },
+
+    {
+        id: 66,
+        nome: "Schweppes Tônica Zero lata 350ml",
+        categoria: "Bebidas",
+        preco: 7.00,
+        imagem: "img/schweppes-tonica-zero.webp"
+    },
+
+    {
+        id: 67,
+        nome: "Soda Limonada lata 350ml",
+        categoria: "Bebidas",
+        preco: 7.00,
+        imagem: "img/soda-limonada.webp"
+    },
+
+    {
+        id: 68,
+        nome: "Sprite lata 350ml",
+        categoria: "Bebidas",
+        preco: 7.00,
+        imagem: "img/sprite-lata.webp"
+    },
+
+
+    // 🍺 CERVEJAS E VINHOS
+
+    {
+        id: 69,
+        nome: "Heineken 600ml",
+        categoria: "Cervejas e Vinhos",
+        preco: 18.00,
+        imagem: "img/heineken-600ml.webp"
+    },
+
+    {
+        id: 70,
+        nome: "Amstel 600ml",
+        categoria: "Cervejas e Vinhos",
+        preco: 16.00,
+        imagem: "img/amstel-600ml.webp"
+    },
+
+    {
+        id: 71,
+        nome: "Original 600ml",
+        categoria: "Cervejas e Vinhos",
+        preco: 16.00,
+        imagem: "img/original-600ml.webp"
+    },
+
+    {
+        id: 72,
+        nome: "Vinho Lunae garrafa",
+        categoria: "Cervejas e Vinhos",
+        preco: 55.00,
+        imagem: "img/vinho-lunae.webp"
+    },
+
+    {
+        id: 73,
+        nome: "Heineken lata",
+        categoria: "Cervejas e Vinhos",
+        preco: 10.00,
+        imagem: "img/heineken-lata.webp"
+    },
+
+    {
+        id: 74,
+        nome: "Amstel lata",
+        categoria: "Cervejas e Vinhos",
+        preco: 10.00,
+        imagem: "img/amstel-lata.webp"
+    }
+
+];
